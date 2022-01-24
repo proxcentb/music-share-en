3779f1d2cbbba3cfbd7d9660fe684964d093ac5d
proxcentb <proxcentb@gmail.com>
2022-01-24 22:44:58 +0300

    redirect to https://proxcentb.github.io/music-share/

--- src/config/helpers.js
+++
@@ -1,4 +0,0 @@
-import '../style.css';
-
-export const Emoji = ({ name, className: _className, ...restProps }) => <img {...restProps} className={`emoji ${_className ?? ''}`} />;
-export const Description = ({ children }) => <div className="description">{children}</div>;
